94126649be8086418b8fbbf8a43344439a4cd4c4
xt <359206421@qq.com>
2024-10-27 21:45:52 +0800

    gitignore

--- node_modules/.pnpm/node_modules/coding/config/webpack.dev.js
+++ node_modules/.pnpm/node_modules/coding/config/webpack.dev.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "../Index.tsx",
+  entry: "../index.tsx",
   output: {
     path: path.resolve(__dirname, "../dist"),
   },
